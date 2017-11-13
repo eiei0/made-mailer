27a9a7a40914eeef7574c51bf22c636a7f123ae7
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2017-11-13 01:57:33 -0600

    Add basic seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.new(first: "Jonathan", last: "MacDonald", email: "jonathan@madelr.com", password: "Apptegy123")
+user.skip_confirmation!
+user.save!
+
+Business.create!(company_name: "The Big Shop", email: "j0nnyappleseed000@mac.com", first: "Chuck E.", last: "Cheese", address: "2101 Sanford Dr.", city: "Little Rock", state: "AR", postal_code: "72227", country: "United States")
