c72945448f736adfc409bb65721f0313c111350e
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2017-11-24 00:12:56 -0600

    Fix active tabs in navbar and separate sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,29 @@ module ApplicationHelper
     link_to title, params.merge(sort: column, direction: direction, page: nil)
       .permit(:sort, :direction, :page), { class: css_class }
   end
+
+  def render_side_bar
+    case controller_name
+    when 'businesses'
+      render 'layouts/shared/businesses_side_bar'
+    when 'reports'
+      render 'layouts/shared/reports_side_bar'
+    end
+  end
+
+  def create_nav_link(text, path)
+    class_name = current_page?(path) ? 'nav-item active' : 'nav-item'
+
+    content_tag(:li, class: class_name) do
+      link_to text, path, { class: 'nav-link' }
+    end
+  end
+
+  def create_subnav_link(text, path)
+    class_name = current_page?(path) ? 'nav-link active' : 'nav-link'
+
+    content_tag(:li, class: 'nav-item') do
+      link_to text, path, { class: class_name }
+    end
+  end
 end
