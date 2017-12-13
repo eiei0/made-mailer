4089e42baad499e3cfad72d39b513fb04960a425
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2017-12-12 23:05:55 -0600

    Remove logic that handles sending bulk mailers

--- app/controllers/mailers_controller.rb
+++ app/controllers/mailers_controller.rb
@@ -1,19 +1,8 @@
 class MailersController < ApplicationController
   def create
-    if params[:business_id].present?
-      business = Business.find(params[:business_id])
-      MailerBuilder.new(business, params[:type]).build
-      flash[:notice] = "#{params[:type].humanize} mailer to #{business.company_name} sent successfully!"
-    elsif params[:business_ids].present?
-      ids = params[:business_ids].split(",").map(&:to_i)
-      businesses = Business.where(id: ids)
-      businesses.each do |business|
-        MailerBuilder.new(business, current_user, params[:type]).build
-      end
-      flash[:notice] = "#{businesses.count} mailers have been sent successfully!"
-    else
-      raise "Please select a business before sending a mailer."
-    end
+    business = Business.find(params[:business_id])
+    MailerBuilder.new(business, params[:type]).build
+    flash[:notice] = "#{params[:type].humanize} mailer to #{business.company_name} sent successfully!"
   rescue => e
     flash[:notice] = "#{e}"
   ensure
