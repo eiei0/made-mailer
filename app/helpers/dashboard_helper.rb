6e0d0d2096b459a740ade567e360dbcdf6250ff0
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2017-12-15 15:11:53 -0600

    Fix arrow icon in Dashboard panels

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,9 +3,7 @@ module DashboardHelper
     link_to path do
       content_tag :div, class: 'panel-footer' do
         concat(content_tag :span, "View Details", class: "pull-left")
-        content_tag :span, class: 'pull-right' do
-          content_tag :i, class: "fa fa-arrow-circle-right"
-        end
+        concat(content_tag(:span, (content_tag :i, "", class: "fa fa-arrow-circle-right"), class: 'pull-right'))
         concat(content_tag :div, "", class: "clearfix")
       end
     end
