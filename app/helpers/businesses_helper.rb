5f6139c79799685c5825d0d2685aa43be8dfcea7
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-01-07 22:00:26 -0600

    Hide business mailer buttons if necessary

---
+++ app/helpers/businesses_helper.rb
@@ -0,0 +1,25 @@
+module BusinessesHelper
+  def render_buttons_panel(business)
+    if show_buttons?(business)
+      render partial: 'buttons', locals: { business: business }
+    else
+      render partial: 'no_options'
+    end
+  end
+
+  def render_followup_mailer_button(business)
+    if business.last_order_placed.present?
+      button_to "send followup mailer", mailers_path(business_id: business.id), { params: { type: "one_month_followup", deliver_now: true }, class: 'btn btn-info' }
+    end
+  end
+
+  def render_stop_mailers_button(business)
+    unless business.responded?
+      button_tag "Stop All Automated Mailers", class: 'btn btn-danger', "data-toggle"=>"modal", 'data-target'=>'#stopBusinessMailers'
+    end
+  end
+
+  def show_buttons?(business)
+    business.last_order_placed.present? || !business.responded?
+  end
+end
