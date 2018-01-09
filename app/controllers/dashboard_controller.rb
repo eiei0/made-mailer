fd410853053ded495f5f04a2da8857a81e42527e
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-01-09 00:48:10 -0600

    Remove new_orders from DashboardController

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@ class DashboardController < ApplicationController
   def index
     @mailers_count = Email.mailers_delivered(1.weeks.ago).count
     @mailers_scheduled = Email.where(scheduled: true).count
-    @new_orders = 0 # Will obviously populate orders from Square here
     @unresponsive = Business.where(status: "unresponsive").where('last_contacted_at > ?', 1.week.ago).count
     @new_responses = Business.where(status: "response_received").where('updated_at > ?', 1.week.ago).count
     @notifications = Notification.last(8).reverse
