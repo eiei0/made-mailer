5b0c163d8810a8ea348fa0a2b9b95be017946792
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-01-16 01:12:05 -0600

    Fix Mailers Secheduled count in dashboard template

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @mailers_count = Email.mailers_delivered(1.weeks.ago).count
-    @mailers_scheduled = Email.where(scheduled: true).count
+    @mailers_count = Email.mailers_delivered(1.week.ago).count
+    @mailers_scheduled = Email.where(scheduled: true).where('delivery_date < ?', 1.week.from_now).count
     @unresponsive = Business.where(status: "unresponsive").where('last_contacted_at > ?', 1.week.ago).count
     @new_responses = Business.where(status: "response_received").where('updated_at > ?', 1.week.ago).count
     @notifications = Notification.last(8).reverse
