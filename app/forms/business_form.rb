313dd9fa70553c36fcec5aa82eb36f4295525889
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-01-27 01:05:50 -0600

    Rubocop fixes for business_form
    - Added top-level class documentation comment
    - Fixed line lengths
    - Replaced double quoted strings with single quoted
    - Add surrounding space for hash rocket operator.

--- app/forms/business_form.rb
+++ app/forms/business_form.rb
@@ -1,9 +1,11 @@
+# Encapsulation of all necessary logic to create and update a Business
 class BusinessForm
   include ActiveModel::Model
 
-  attr_reader :business_params, :deliver_now, :delivery_date, :company_name, :email
-  attr_accessor :company_name, :email, :first, :last, :url, :delivery_date, :notes,
-    :deliver_now, :address, :city, :state, :postal_code, :country, :status, :phone
+  attr_reader   :business_params
+  attr_accessor :company_name, :email, :first, :last, :url, :delivery_date,
+                :notes, :deliver_now, :address, :city, :state, :postal_code,
+                :country, :status, :phone
 
   # validates :business_params, presence: true, if: :business_params_valid?
   validates :delivery_date, presence: true, if: :scheduled?
@@ -26,11 +28,15 @@ class BusinessForm
   private
 
   def create_business
-    Business.create!(business_params.merge("status"=>business_params[:status].underscore))
+    Business.create!(
+      business_params.merge('status' => business_params[:status].underscore)
+    )
   end
 
   def build_emails(business)
-    builder = MailerBuilder.new(business, 'initial_intro', deliver_now, delivery_date)
+    builder = MailerBuilder.new(
+      business, 'initial_intro', deliver_now, delivery_date
+    )
     builder.build!
   end
 
@@ -38,8 +44,9 @@ class BusinessForm
     deliver_now == false && delivery_date.present?
   end
 
-  # def business_params_valid?
-  #   required_keys = [:company_name, :email]
-  #   required_keys.all? { |k| business_params.key?(k) && business_params[k].present? }
-  # end
+  def business_params_valid?
+    required_keys = %i[company_name email]
+    required_keys
+      .all? { |k| business_params.key?(k) && business_params[k].present? }
+  end
 end
