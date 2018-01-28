d396c4db9b9706650031fa025fa3a067dade0aa4
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-01-28 14:10:54 -0600

    Rubocop fixes for InboundEmailHandler

--- app/services/inbound_email_handler.rb
+++ app/services/inbound_email_handler.rb
@@ -12,39 +12,44 @@ class InboundEmailHandler
     businesses = fetch_businesses
 
     businesses.each do |biz|
-      email = create_incoming_email(biz)
-
       biz.cancel_mailers(biz.all_jids) if biz.scheduled?
-      biz.update!(status: "response_received") if biz.mailer_phase.present?
-      notify!(biz, email) if biz.responded? && !biz.notified?
+      biz.update!(status: 'response_received') if biz.mailer_phase.present?
+      notify!(biz) if already_notified?(biz)
     end
   rescue => e
-    raise "#{e}"
+    raise e.to_s
   end
 
   private
 
   def fetch_businesses
     search = new_messages.flat_map do |msg|
-      email_address = Mail::Address.new(msg.from.first.downcase)
-      domain = email_address.domain
-      Email::COMMON_DOMAINS.include?(domain) ? "%#{email_address}%" : "%@#{domain}%"
+      if Email::COMMON_DOMAINS.include?(domain)
+        email_address = Mail::Address.new(msg.from.first.downcase)
+        "%#{email_address}%"
+      else
+        "%@#{domain}%"
+      end
     end
 
-    Business.where("email ILIKE ANY ( array[?] )", search)
+    Business.where('email ILIKE ANY ( array[?] )', search)
   end
 
   def create_incoming_email(business)
     business.emails.create!(
-      classification: "inbound",
+      classification: 'inbound',
       scheduled: false,
       jid: nil,
       delivery_date: nil
     )
   end
 
-  def notify!(business, email)
+  def notify!(business)
     Email.notify_admin(business)
-    business.create_notification!(business.company_name, "fa-reply-all")
+    business.create_notification!(business.company_name, 'fa-reply-all')
+  end
+
+  def already_notified?(biz)
+    biz.responded? && !biz.notified?
   end
 end
