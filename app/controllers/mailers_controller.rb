8cc5c3550c29987f6a7c973c182d444321e84ae7
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-01-28 12:33:14 -0600

    Rubocop fixes for MailersController

--- app/controllers/mailers_controller.rb
+++ app/controllers/mailers_controller.rb
@@ -1,24 +1,28 @@
 # Http requests for all mailers
 class MailersController < ApplicationController
-  before_action :fetch_business, only: [:create, :destroy]
+  before_action :fetch_business, only: %i[create destroy]
 
   def create
-    builder = MailerBuilder.new(@business, params[:type], params[:deliver_now], params[:delivery_date])
-
-    flash[:notice] = "#{params[:type].humanize} mailer to #{@business.company_name} sent successfully!" if builder.build!
+    if MailerBuilder.new(*builder_params).build!
+      flash[:notice] =
+        "#{params[:type].humanize} mailer sent to #{@business.company_name}."
+    end
   rescue => e
-    flash[:notice] = "#{e}"
+    flash[:notice] = e.to_s
   ensure
     redirect_back(fallback_location: root_path)
   end
 
   def destroy
     stopped = @business.stop_email_flow
-    @business.update!(status: "pending", mailer_phase: nil)
+    @business.update!(status: 'pending', mailer_phase: nil)
 
-    flash[:notice] = "All automated mailers for #{@business.company_name} were stopped successfully." if stopped
+    if stopped
+      flash[:notice] =
+        "All automated mailers stopped for #{@business.company_name}."
+    end
   rescue => e
-    flash[:notice] = "#{e}"
+    flash[:notice] = e.to_s
   ensure
     redirect_back(fallback_location: root_path)
   end
@@ -27,5 +31,9 @@ class MailersController < ApplicationController
 
   def fetch_business
     @business = Business.find(params[:business_id])
+  end
+
+  def builder_params
+    [@business, params[:type], params[:deliver_now], params[:delivery_date]]
   end
 end
