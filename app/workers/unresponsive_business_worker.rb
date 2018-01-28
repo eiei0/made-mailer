82acf6ebc93b38ddb8561acedbb575ebc9f2e168
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-01-27 21:30:25 -0600

    Rubocop fixes for unresponsive biz worker

--- app/workers/unresponsive_business_worker.rb
+++ app/workers/unresponsive_business_worker.rb
@@ -7,14 +7,13 @@ class UnresponsiveBusinessWorker
 
   def perform
     businesses = Business
-      .where(mailer_phase: "second_follow_up")
-      .select { |b| DateTime.now > b.last_contacted_at + 2.weeks }
+                 .where(mailer_phase: 'second_follow_up')
+                 .select { |b| DateTime.now > b.last_contacted_at + 2.weeks }
 
-    if businesses.present?
-      businesses.map { |b|
-        b.update_attribute :status, "unresponsive"
-        b.create_notification!(b.company_name, "fa-thumbs-down")
-      }
+    return unless businesses.present?
+    businesses.map do |b|
+      b.update_attribute :status, 'unresponsive'
+      b.create_notification!(b.company_name, 'fa-thumbs-down')
     end
   end
 end
