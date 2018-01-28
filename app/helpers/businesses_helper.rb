a23c940c3bab7d4697033378f6ad4da029087797
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-01-28 15:13:59 -0600

    Rubocop fixes for BusinessHelper

--- app/helpers/businesses_helper.rb
+++ app/helpers/businesses_helper.rb
@@ -9,15 +9,23 @@ module BusinessesHelper
   end
 
   def render_followup_mailer_button(business)
-    if business.last_order_placed.present?
-      button_to "send followup mailer", mailers_path(business_id: business.id), { params: { type: "post_purchase_check_in", deliver_now: true }, class: 'btn btn-info' }
-    end
+    return unless business.last_order_placed.present?
+    button_to(
+      'send followup mailer',
+      mailers_path(business_id: business.id),
+      params: { type: 'post_purchase_check_in', deliver_now: true },
+      class: 'btn btn-info'
+    )
   end
 
   def render_stop_mailers_button(business)
-    unless business.scheduled? || business.responded?
-      button_tag "Stop All Automated Mailers", class: 'btn btn-danger', "data-toggle"=>"modal", 'data-target'=>'#stopBusinessMailers'
-    end
+    return unless business.scheduled? || business.responded?
+    button_tag(
+      'Stop All Automated Mailers',
+      class: 'btn btn-danger',
+      'data-toggle' => 'modal',
+      'data-target' => '#stopBusinessMailers'
+    )
   end
 
   def show_buttons?(business)
@@ -25,27 +33,33 @@ module BusinessesHelper
   end
 
   def render_status_type(business)
-    if business.status?
-      button_to "#{business.status.humanize.titleize}", business_path(business), method: :get, class: "btn btn-sm btn-outline #{status_type(business)}"
-    end
+    return if business.status?
+    button_to(
+      business.status.humanize.titleize.to_s,
+      business_path(business),
+      method: :get,
+      class: "btn btn-sm btn-outline #{status_type(business)}"
+    )
   end
 
+  # rubocop:disable Metrics/MethodLength
   def status_type(business)
     case business.status
-    when "pending"
-      "btn-warning"
-    when "current_wholesale_vendor"
-      "btn-success"
-    when "current_consignment_vendor"
-      "btn-success"
-    when "declined"
-      "btn-danger"
-    when "followup_later"
-      "btn-primary"
-    when "response_received"
-      "btn-info"
-    when "unresponsive"
-      "btn-danger"
+    when 'pending'
+      'btn-warning'
+    when 'current_wholesale_vendor'
+      'btn-success'
+    when 'current_consignment_vendor'
+      'btn-success'
+    when 'declined'
+      'btn-danger'
+    when 'followup_later'
+      'btn-primary'
+    when 'response_received'
+      'btn-info'
+    when 'unresponsive'
+      'btn-danger'
     end
   end
+  # rubocop:enable Metrics/MethodLength
 end
