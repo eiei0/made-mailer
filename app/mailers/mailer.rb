a73197550c6b35b56d083ec714d816bbd560dc26
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-02-05 09:01:59 -0600

    Update mailer templates

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -3,37 +3,41 @@ class Mailer < ApplicationMailer
 
   def initial_intro(business)
     @business_name = business.company_name
+    @contact_first = business.first
     @stacy_email = ENV["stacy_email"]
     @stacy_phone = ENV["stacy_phone"]
 
     attachments['made_linesheet.pdf'] = File.read('db/data/made_linesheet.pdf')
-    mail(to: business.email, subject: "Arkansas Quartz Jewelry")
+    mail(to: business.email, subject: 'I mine Quartz from the ground and turn it into jewelry!')
   end
 
   def first_follow_up(business)
     @business_name = business.company_name
+    @contact_first = business.first
     @stacy_email = ENV["stacy_email"]
     @stacy_phone = ENV["stacy_phone"]
 
     attachments['made_linesheet.pdf'] = File.read('db/data/made_linesheet.pdf')
-    mail(to: business.email, subject: "Arkansas Quartz Jewelry")
+    mail(to: business.email, subject: 'Touching base about Arkansas Quartz jewelry')
   end
 
   def second_follow_up(business)
     @business_name = business.company_name
+    @contact_first = business.first
     @stacy_email = ENV["stacy_email"]
     @stacy_phone = ENV["stacy_phone"]
 
     attachments['made_linesheet.pdf'] = File.read('db/data/made_linesheet.pdf')
-    mail(to: business.email, subject: "Arkansas Quartz Jewelry")
+    mail(to: business.email, subject: 'Hand mined, Handmade Quartz jewelry follow-up')
   end
 
   def post_purchase_check_in(business)
     @business_name = business.company_name
+    @wholesale_email = ENV['wholesale_email']
     @stacy_email = ENV["stacy_email"]
     @stacy_phone = ENV["stacy_phone"]
 
-    mail(to: business.email, subject: "Order Followup")
+    mail(to: business.email, subject: "Is it time to restock your made. display?")
   end
 
   def admin_response_notification(business)
