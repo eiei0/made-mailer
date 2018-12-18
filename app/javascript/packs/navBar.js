f3be84bb33d350b61be171becb62c211c7e8b208
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-12-17 21:06:15 -0600

    Add vuetify

--- app/javascript/packs/navBar.js
+++ app/javascript/packs/navBar.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
 import App from "../components/nav_bar/App";
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
 
 document.addEventListener("DOMContentLoaded", () => {
   const app = new Vue({
