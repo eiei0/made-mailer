e32b86b92a1ff6c72501ed8bc5fb1eb9ca27b7e4
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2018-12-17 19:29:16 -0600

    Run install webpacker script

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ MAILER_SETTINGS = Hash[
 PREVIEW_PATH = Rails.root.join("spec", "mailers", "previews")
 
 Rails.application.configure do
+  # Verifies that versions and hashed value of the package contents in the project's package.json
+config.webpacker.check_yarn_integrity = true
+
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
